aafbff4af578b0134f84ee76ad49f350c8125820
SagaO6 <cabaleirothiago32@gmail.com>
2023-11-09 11:47:05 -0300

    inner join e soma

--- scriptSQl.sql
+++ scriptSQl.sql
@@ -6,9 +6,6 @@ show tables;
 use dbHotel;
 
 
-
-
-
 create table funcionarios (
 	idFunc int primary key auto_increment,
     nomeFunc varchar (100) not null,
@@ -78,6 +75,12 @@ insert into Quartos (andar, numeroQuarto, tipoQuarto, ocupacaoMax, situacao, nom
 ftigobar abastecido e banheiro com secador de cabelo e amenities e mesa de trabalho", "https://cf.bstatic.com/xdata/images/hotel/max1024x768/427861759.
 jpg?k=8e32bd8bf0f4e883b632dc439164cad3e5fd7059a729b780ccfdb278584a311b&o=&hp=1", 1000.00, "Não", "", "Queen Size", "sim");
 
+insert into Quartos (andar, numeroQuarto, tipoQuarto, ocupacaoMax, situacao, nome, descricao, foto, preco, cafeDaManha, precoCafe, tipoCama, varanda) values 
+("2º", "207", "Deluxe", 3, "não", "Familiar", 
+"O quarto de 32m² com piso frio, varanda - vista para o mar. Oferece ar condicionado individual, TV LCD 42, wi-fi grátis, cofre digital, 
+ftigobar abastecido e banheiro com secador de cabelo e amenities e mesa de trabalho", "https://cf.bstatic.com/xdata/images/hotel/max1024x768/427861759.
+jpg?k=8e32bd8bf0f4e883b632dc439164cad3e5fd7059a729b780ccfdb278584a311b&o=&hp=1", 3000.00, "sim", "70.00", "Queen Size", "sim");
+
 select * from Quartos;
 
 
@@ -119,7 +122,7 @@ foreign key (idCliente) references clientes (idCliente)
 describe Pedidos;
 
 insert into Pedidos (statusPedido, idCliente) values ("Pendente", 1);
-insert into Pedidos (statusPedido, idCliente) values ("Finalizado", 2);
+insert into Pedidos (statusPedido, idCliente) values ("Pendente", 2);
 
 select * from Pedidos;
 
@@ -143,136 +146,24 @@ describe Reserva;
 insert into Reserva (idPedido, idQuarto, checkin, checkout) values (1, 4, "2023-11-02 14:00:00", "2023-11-05 12:00:00");
 insert into Reserva (idPedido, idQuarto, checkin, checkout) values (1, 11, "2023-11-02 14:00:00", "2023-11-05 12:00:00");
 
-select * from Pedidos inner join clientes on Pedidos.idClientes = clientes.idClientes;
+insert into Reserva (idPedido, idQuarto, checkin, checkout) values (2, 9, "2023-11-02 14:00:00", "2023-11-05 12:00:00");
+
+
 select * from Reserva;
-select Reserva.idReserva, Pedidos.idPedido, Quartos.idQuarto, Quartos.nome, Quartos.andar, Quartos.numeroQuarto 
-from (Reserva inner join Pedidos on Reserva.idPedido = Pedidos.idPedido) inner join quartos on Reserva.idQuarto = Quartos.idQuarto;
+select clientes.nomeCompleto, clientes.cpf, clientes.email, Pedidos.idPedido, Pedidos.dataPedido, Quartos.tipoQuarto, Quartos.nome, Quartos.andar,
+Quartos.numeroQuarto, Quartos.preco, Reserva.checkin, Reserva.checkout 
+from (Reserva inner join Pedidos on Reserva.idPedido = Pedidos.idPedido) inner join Quartos on Reserva.idQuarto = Quartos.idQuarto;
 
+select clientes.nomeCompleto, clientes.cpf, clientes.email, Pedidos.idPedido, Pedidos.dataPedido, Quartos.tipoQuarto, Quartos.nome, Quartos.andar,
+Quartos.numeroQuarto, Quartos.preco, Reserva.checkin, Reserva.checkout from clientes
+inner join Pedidos on clientes.idCliente = Pedidos.idCliente 
+inner join Reserva on Reserva.idPedido = Pedidos.idPedido 
+inner join Quartos on Reserva.idQuarto = Quartos.idQuarto;
 
+select sum(Quartos.preco) as total from Reserva inner join Quartos on Reserva.idQuarto = Quartos.idQuarto where idPedido = 1;
 
-
-
-
-
-
-
-
-create database dbPadaria;
-
-use dbPadaria;
-
-create table Fornecedores(
-idFornecedor int primary key auto_increment,
-nomeFornecedor varchar(50) not null,
-cnpjFornecedor varchar(20) not null,
-telefoneFornecedor varchar(20) not null,
-emailFornecedor varchar(50) not null unique,
-cep varchar(9),
-enderecoFornecedor varchar(100), 
-numeroEndereco varchar(10),
-bairro varchar(40),
-cidade varchar(40),
-estado char(2)
-
-);
-
-describe Fornecedores;
-
-insert into Fornecedores (nomeFornecedor, cnpjFornecedor, telefoneFornecedor, emailFornecedor, cep, enderecoFornecedor, numeroEndereco, bairro, cidade,
-estado) values ("Rafaela", "76.172.127/0001-50", "(11)968574521", "Rafaela@gmail.com", "18600-270", "Rua Doutor Antônio Carlos de Abreu Sodré", "25",
-"Centro", "Botucatu", "SP");
-
-select * from Fornecedores;
-
-
-
-create table Produtos(
-idProduto int primary key auto_increment,
-nomeProduto varchar(50) not null,
-descricaoProduto text,
-precoProduto decimal(10,2) not null,
-estoqueProduto int not null,
-categoriaProduto enum ("Pães","Bolos", "confeitaria", "salgados"),
-idFornecedor int not null,
-foreign key (idFornecedor) references Fornecedores (idFornecedor)
-
-);
-
-describe Produtos;
-
-insert into Produtos (nomeProduto, descricaoProduto, precoProduto, estoqueProduto, categoriaProduto, idFornecedor) values ("coxinha", 
-"massa de farinha de trigo recheada com um delicioso recheio de frango desfiado, catupiry (um tipo de requeijão cremoso), 
-temperos e, às vezes, outros ingredientes, como ervilhas e milho.", 2.00, "100", "salgados", 1);
-
-insert into Produtos (nomeProduto, descricaoProduto, precoProduto, estoqueProduto, categoriaProduto, idFornecedor) values ("pão", "Ele é feito 
-a partir de uma mistura de ingredientes simples, principalmente farinha de trigo, água, sal e fermento. Aqui está uma descrição geral de um pão"
-, 0.60, "10", "Pães", 1);
-
-select * from Produtos;
-select * from Produtos where categoriaProduto = "Pães";
-select * from Produtos where precoProduto < 50.00 order by precoProduto asc;
-
-create table Clientes(
-idCliente int primary key auto_increment,
-nomeCliente varchar(50),
-cpfCliente varchar(15) not null unique,
-telefoneCliente varchar(20),
-emailCliente varchar(50) unique,
-cep varchar(9),
-enderecoCliente varchar(100),
-numeroEndereco varchar(10),
-bairro varchar(40),
-cidade varchar(40),
-estado char(2)
-
-);
-
-describe Clientes;
-
-insert into Clientes (nomeCliente, cpfCliente, telefoneCliente, emailCliente, cep, enderecoCliente, numeroEndereco, bairro, cidade, estado) values 
-("Roger", "844.441.178-77", "(11)935354318", "roger@gmail.com", "03415-120", "Rua João Tursi", "19", "Vila Tijuco", "Guarulhos", "SP");
-
-select * from Clientes;
-
-create table Pedidos(
-idPedido int primary key auto_increment,
-dataPedido timestamp default current_timestamp,
-statusPedido enum("Pendente", "Finalizado", "Cancelado"),
-idCliente int not null,
-foreign key (idCliente) references Clientes (idCliente)
-
-);
-
-describe Pedidos;
-
-insert into Pedidos (statusPedido, idCliente) values ("Finalizado", 1);
-
-select * from Pedidos inner join Clientes on Pedidos.idCliente = Clientes.idCliente;
-select * from Pedidos;
-
-create table itensPedidos(
-iditensPedidos int primary key auto_increment,
-idPedido int not null,
-foreign key (idPedido) references Pedidos(idPedido),
-idProduto int not null,
-foreign key (idProduto) references Produtos(idProduto),
-quantidade int not null
-
-);
-
-describe itensPedidos;
-
-insert into itensPedidos (idPedido, idProduto, quantidade) values (1, 2, 5);
-
-delete from itensPedidos where iditensPedidos = 2; 
-
-select * from itensPedidos;
-select itensPedidos.iditensPedidos, Pedidos.idPedido, Produtos.idProduto, Produtos.nomeProduto, Produtos.precoProduto, Produtos.categoriaProduto
-from (itensPedidos inner join Pedidos on itensPedidos.idPedido = Pedidos.idPedido) inner join Produtos on itensPedidos.idProduto = Produtos.idProduto;
-
-
-
-
-select Clientes.nomeCliente, Pedidos.idPedido, Pedidos.dataPedido, itensPedidos.quantidade, Produtos.nomeProduto, Produtos.precoProduto 
-from (Pedidos inner join Clientes on Pedidos.idCliente = Clientes.idCliente) inner join itensPedidos on Pedidos.idPedido = itensPedidos.idPedido inner join 
-Produtos on Produtos.idProduto = itensPedidos.idProduto;
+select clientes.nomeCompleto, clientes.cpf, Pedidos.idPedido, Quartos.tipoQuarto, Quartos.nome, Quartos.andar, Quartos.numeroQuarto,  sum(Quartos.preco) as total from clientes 
+inner join Pedidos on clientes.idCliente = Pedidos.idCliente 
+inner join Reserva on Reserva.idPedido = Pedidos.idPedido 
+inner join Quartos on Reserva.idQuarto = Quartos.idQuarto where Pedidos.idPedido = 1
+group by clientes.nomeCompleto, clientes.cpf, Pedidos.idPedido, Quartos.tipoQuarto, Quartos.nome, Quartos.andar, Quartos.numeroQuarto;
